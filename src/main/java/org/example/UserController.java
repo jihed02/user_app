fc45ceb843d39e62b2848a1f83124aa567fa5611
jihedbenamara <jihedbenamara02@gmail.com>
2025-10-20 00:05:18 +0100

    main commit

--- src/main/java/org/example/UserController.java
+++ src/main/java/org/example/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/userss")
 @CrossOrigin(origins = "*")
 public class UserController {
 
